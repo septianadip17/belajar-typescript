5d7a6e9733e97bac138d740dfea8f9eef93859a4
Septian Adi Pratama <septianadip7998@gmail.com>
2025-12-08 21:53:11 +0700

    array angka

--- latihan/integer/arrayAngka.ts
+++ latihan/integer/arrayAngka.ts
@@ -1,2 +1,17 @@
 // Function menerima array angka, lalu return total semua isinya.
 
+function isiArray(arr: number[]):number | null{
+  let total: number = 0
+  if (arr.length == 0){
+    return null
+  }
+  for (const num of arr){
+    total += num
+  }
+  return total
+}
+
+const angkaSaya: number[] = []
+const hasilAngka = isiArray(angkaSaya)
+
+console.log(hasilAngka)
