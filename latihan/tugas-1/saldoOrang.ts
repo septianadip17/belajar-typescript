e607698d2d7afa8ad8a7dae5ca80e4c6924064fb
Septian Adi Pratama <septianadip7998@gmail.com>
2025-12-09 19:10:39 +0700

    saldo orang

--- latihan/tugas-1/saldoOrang.ts
+++ latihan/tugas-1/saldoOrang.ts
@@ -1,10 +1,12 @@
-import { User } from "./User";
+// import { User } from "./User";
 
-let Users: User[] = [
-  { id: 1, name: "Andi", balance: 100000 },
-  { id: 2, name: "Budi", balance: 50000 },
-  { id: 3, name: "Citra", balance: 200000 },
-];
+interface User {
+  id: number;
+  name: string;
+  balance: number;
+}
+
+
 
 function getUserById(id: number): User | null {
   for (const user of Users) {
@@ -15,5 +17,10 @@ function getUserById(id: number): User | null {
   return null
 }
 
-const idUser = getUserById(1)
+let Users: User[] = [
+  { id: 1, name: "Andi", balance: 100000 },
+  { id: 2, name: "Budi", balance: 50000 },
+  { id: 3, name: "Citra", balance: 200000 },
+];
+const idUser = getUserById(a)
 console.log(idUser)
