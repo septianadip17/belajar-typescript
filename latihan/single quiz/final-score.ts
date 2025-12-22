8629cc2f9b930d1547e6329c47d3b255ad145db6
Septian Adi Pratama <septianadip7998@gmail.com>
2025-12-22 22:39:21 +0700

    final score

---
+++ latihan/single quiz/final-score.ts
@@ -0,0 +1,7 @@
+function getFinalScore(score: number, bonus: number, penalty: number): number | string{
+  let finalScore = 0
+  if(score < 0 || bonus < 0 || penalty < 0){
+    "invalid input"
+  }
+  return ""
+}
