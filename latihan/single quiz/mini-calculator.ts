37ea0c48eae15e2a75ecc1a96975ce7882b2a126
Septian Adi Pratama <septianadip7998@gmail.com>
2025-12-22 22:35:20 +0700

    calculator ok

--- latihan/single quiz/mini-calculator.ts
+++ latihan/single quiz/mini-calculator.ts
@@ -1,3 +1,37 @@
 function calculateResult(a: number, b: number, operation: string): number | string {
-return 0
+  if (operation === "+") {
+    return a + b
+  } else if (operation === "*") {
+    return a * b
+  } else if (operation === "-") {
+    return a - b
+  } else if (operation === "/") {
+    if (b === 0) {
+      "can not divide by zero"
+    }
+    return a / b
+  }
+  return "invalid operation"
 }
+
+// penjumlahan
+const penjumlahan = calculateResult(1, 2, "+")
+console.log(`penjumlahan ${penjumlahan}`)
+
+// perkalian
+const perkalian = calculateResult(1, 2, "*")
+console.log(`perkalian ${perkalian}`)
+
+// pengurangan
+const pengurangan = calculateResult(2, 1, "-")
+console.log(`pengurangan ${pengurangan}`)
+
+// pembagian
+const pembagianPertama = calculateResult(4, 2, "/")
+const pembagianKedua = calculateResult(2, 0, "/")
+console.log(`pembagian pertama ${pembagianPertama}`)
+console.log(`pembagian kedua ${pembagianKedua}`)
+
+// invalid operation
+const ngasal = calculateResult(1, 2, ";")
+console.log(`ngasal ${ngasal}`)
