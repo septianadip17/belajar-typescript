5eaf4e7a0300b530644cd06b4443946a50c088e9
Septian Adi Pratama <septianadip7998@gmail.com>
2025-12-25 22:27:47 +0700

    hold money feature

--- latihan/tugas-4/bank.ts
+++ latihan/tugas-4/bank.ts
@@ -3,12 +3,12 @@ type Account = {
   owner: string;
   balance: number;
   holdBalance: number;
-}
+};
 
 let accounts: Account[] = [
   { id: 1, owner: "Alice", balance: 1000000, holdBalance: 0 },
   { id: 2, owner: "Bob", balance: 500000, holdBalance: 0 },
-]
+];
 
 enum TransactionStatus {
   HOLD = "HOLD",
@@ -17,14 +17,16 @@ enum TransactionStatus {
 }
 
 type Transaction = {
-  id: number; fromId: number;
-  toId: number; amount: number; fee: number;
+  id: number;
+  fromId: number;
+  toId: number;
+  amount: number;
+  fee: number;
   status: TransactionStatus;
-}
+};
 
 let transactions: Transaction[] = [];
 let transactionCounter = 1;
-
 
 // function get account by id
 function getAccountById(accountId: number): Account | undefined {
@@ -47,14 +49,64 @@ function checkBalance(accountId: number): number | string {
   return "Account not found";
 }
 
+// function hold money
+function holdMoney(fromId: number, toId: number, amount: number): string | Transaction {
+  if (fromId === toId) {
+    return "Cannot transfer to same account";
+  }
+  if (amount <= 0) {
+    return "Invalid amount";
+  }
 
+  let sender: Account | undefined;
+  let receiver: Account | undefined;
 
+  for (const account of accounts) {
+    if (account.id === fromId) {
+      sender = account;
+    }
+    if (account.id === toId) {
+      receiver = account;
+    }
+    // account not found validation
+  }
+  if (!sender || !receiver) {
+    return "Account not found";
+  }
+  const percentageFee = amount * 0.02;
+  const fee = Math.max(percentageFee, 2000);
+  const totalHold = amount + fee;
 
+  const availableBalance = sender.balance - sender.holdBalance;
+
+  if (availableBalance < totalHold) {
+    return "Insufficient balance";
+  }
+  sender.holdBalance += totalHold;
+
+  const newTransaction: Transaction = {
+    id: transactionCounter,
+    fromId: fromId,
+    toId: toId,
+    amount: amount,
+    fee: fee,
+    status: TransactionStatus.HOLD,
+  };
+
+  transactions.push(newTransaction);
+  transactionCounter++;
+
+  return newTransaction;
+}
 
 // get account by account id
-const getAccount = getAccountById(2)
-console.log(getAccount)
+const getAccount = getAccountById(2);
+console.log(getAccount);
 
 // check balance by account id
-const userBalance = checkBalance(1)
-console.log(userBalance)
+const userBalance = checkBalance(1);
+console.log(userBalance);
+
+// hold money
+const trx1 = holdMoney(1, 3, 100000);
+console.log(trx1);
