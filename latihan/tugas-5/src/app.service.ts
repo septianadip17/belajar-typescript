7fa413f645dab254f922d6df60cba7406c2f5d92
Septian Adi Pratama <septianadip7998@gmail.com>
2026-01-04 10:35:19 +0700

    set up service

--- latihan/tugas-5/src/app.service.ts
+++ latihan/tugas-5/src/app.service.ts
@@ -1,8 +1,33 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
-
+import { DataSource } from 'typeorm';
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private dataSource: DataSource) { }
+
+  async getHello(): Promise<string> {
     return 'Hello World!';
+  }
+
+  async checkDatabaseConnection() {
+    try {
+      const result = await this.dataSource.query('SELECT 1 + 1 AS sum');
+
+      console.log('Hasil Query:', result);
+      return {
+        status: 'OK',
+        message: 'Koneksi Database Berhasil!',
+        data: result,
+      };
+    } catch (error) {
+      return {
+        status: 'Error',
+        message: 'Gagal konek ke database',
+        error: error.message,
+      };
+    }
   }
 }
