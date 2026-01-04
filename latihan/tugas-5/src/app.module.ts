4bfa6b12a602d1d47c7fcd04690c039e58496b27
Septian Adi Pratama <septianadip7998@gmail.com>
2026-01-04 10:33:24 +0700

    set up module

--- latihan/tugas-5/src/app.module.ts
+++ latihan/tugas-5/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Import ini
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'rootpassword',
+      database: 'coba_nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
